ca845b9485fcc5188aa8b7dfee14bb8d8015b038
dpendergast <dlp75@case.edu>
2021-09-18 01:28:20 -0400

    tweaking game class some more

--- raycast/main.py
+++ raycast/main.py
@@ -1,3 +1,8 @@
+
+############## game.py ##############
+# TODO web exports only support a single file right now (I think)
+
+
 import katagames_sdk.engine as kataen
 
 import time
@@ -6,20 +11,22 @@ import collections
 pygame = kataen.import_pygame()
 EventReceiver = kataen.EventReceiver
 EngineEvTypes = kataen.EngineEvTypes
-scr_size = None
 
 
 class Game:
+    """Base class for games."""
 
     def __init__(self, track_fps=True):
         self._fps_n_frames = 10 if track_fps else 0
         self._fps_tracker_logic = collections.deque()
         self._fps_tracker_rendering = collections.deque()
+        self._tick = 0
 
     def start(self):
-        kataen.init(self.get_mode())
+        """Starts the game loop. This method will not exit until the game has finished execution."""
+        kataen.init(self._get_mode_internal())
 
-        li_recv = [kataen.get_game_ctrl(), GameViewController(self)]
+        li_recv = [kataen.get_game_ctrl(), self.build_controller()]
         for recv_obj in li_recv:
             recv_obj.turn_on()
 
@@ -28,38 +35,26 @@ class Game:
         li_recv[0].loop()
         kataen.cleanup()
 
-    def render_internal(self, screen):
-        if self._fps_n_frames > 0:
-            self._fps_tracker_rendering.append(time.time())
-            if len(self._fps_tracker_rendering) > self._fps_n_frames:
-                self._fps_tracker_rendering.popleft()
-        self.render(screen)
+    def get_mode(self) -> str:
+        """returns: "HD', 'OLD_SCHOOL', or 'SUPER_RETRO'"""
+        return 'OLD_SCHOOL'
 
-    def update_internal(self, events, dt):
-        if self._fps_n_frames > 0:
-            self._fps_tracker_logic.append(time.time())
-            if len(self._fps_tracker_logic) > self._fps_n_frames:
-                self._fps_tracker_logic.popleft()
-        self.update(events, dt)
-
-    def get_mode(self):
-        """returns: kataen.HD_MODE, kataen.OLD_SCHOOL_MODE, or kataen.SUPER_RETRO_MODE"""
-        return kataen.OLD_SCHOOL_MODE
-
-    def is_running_in_web(self):
+    def is_running_in_web(self) -> bool:
         return kataen.runs_in_web()
+
+    def get_tick(self) -> int:
+        return self._tick
 
     def pre_update(self):
         pass
 
     def render(self, screen):
-        screen.fill(pygame.color.Color('antiquewhite2'))
-        pygame.draw.circle(screen, (244, 105, 251), [240, 135], 15, 0)
+        raise NotImplementedError()
 
     def update(self, events, dt):
-        print("LOGIC_FPS={}, RENDER_FPS={}".format(self.get_fps(logical=True), self.get_fps(logical=False)))
+        raise NotImplementedError()
 
-    def get_fps(self, logical=True):
+    def get_fps(self, logical=True) -> float:
         q = self._fps_tracker_logic if logical else self._fps_tracker_rendering
         if len(q) <= 1:
             return 0
@@ -71,34 +66,89 @@ class Game:
             else:
                 return (n_frames - 1) / total_time_secs
 
+    def _render_internal(self, screen):
+        if self._fps_n_frames > 0:
+            self._fps_tracker_rendering.append(time.time())
+            if len(self._fps_tracker_rendering) > self._fps_n_frames:
+                self._fps_tracker_rendering.popleft()
+        self.render(screen)
 
-class GameViewController(EventReceiver):
+    def _update_internal(self, events, dt):
+        if self._fps_n_frames > 0:
+            self._fps_tracker_logic.append(time.time())
+            if len(self._fps_tracker_logic) > self._fps_n_frames:
+                self._fps_tracker_logic.popleft()
+        self.update(events, dt)
+        self._tick += 1
 
-    def __init__(self, game):
-        super().__init__()
-        self._game = game
-        self._event_queue = []
-        self._last_update_time = time.time()
-
-    def proc_event(self, ev, source):
-        if ev.type == EngineEvTypes.PAINT:
-            self._game.render_internal(ev.screen)
-        elif ev.type == EngineEvTypes.LOGICUPDATE:
-            cur_time = time.time()
-            self._game.update_internal(self._event_queue, cur_time - self._last_update_time)
-            self._last_update_time = cur_time
-            self._event_queue.clear()
+    def _get_mode_internal(self):
+        mode_str = self.get_mode().upper()
+        if mode_str == 'HD':
+            return kataen.HD_MODE
+        elif mode_str == 'OLD_SCHOOL':
+            return kataen.OLD_SCHOOL_MODE
+        elif mode_str == 'SUPER_RETRO':
+            return kataen.SUPER_RETRO_MODE
         else:
-            self._event_queue.append(ev)
+            raise ValueError("Unrecognized mode: {}".format(mode_str))
+
+    class _GameViewController(EventReceiver):
+        def __init__(self, game):
+            super().__init__()
+            self._game = game
+            self._event_queue = []
+            self._last_update_time = time.time()
+
+        def proc_event(self, ev, source):
+            if ev.type == EngineEvTypes.PAINT:
+                self._game._render_internal(ev.screen)
+            elif ev.type == EngineEvTypes.LOGICUPDATE:
+                cur_time = time.time()
+                self._game._update_internal(self._event_queue, cur_time - self._last_update_time)
+                self._last_update_time = cur_time
+                self._event_queue.clear()
+            else:
+                self._event_queue.append(ev)
+
+    def build_controller(self) -> EventReceiver:
+        return Game._GameViewController(self)
 
 
-# entry point for packaged web game
+############## game.py ##############
+
+############## raycaster.py ##############
+
+
+class RayCasterGame(Game):
+
+    def __init__(self):
+        super().__init__()
+
+    def get_mode(self):
+        return 'SUPER_RETRO'
+
+    def update(self, events, dt):
+        if self.get_tick() % 20 == 0:
+            dims = kataen.get_screen().get_size()
+            cap = "Raycaster (FPS={:.1f}, DIMS={})".format(self.get_fps(logical=False), dims)
+            pygame.display.set_caption(cap)
+
+    def render(self, screen):
+        pass
+
+############## raycaster.py ##############
+
+############## main.py ##############
+
+
 def run_game():
-    game = Game()
-    game.start()
+    """Entry point for packaged web runs"""
+    g = RayCasterGame()
+    g.start()
 
 
-# entry point for offline runs
 if __name__ == '__main__':
+    """Entry point for offline runs"""
     run_game()
-    
+
+############## main.py ##############
